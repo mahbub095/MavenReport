7b0e35d88fd35a04a8537db41bbfcfebf910209a
Mahbub <mahbub.095@gmail.com>
2021-03-11 16:17:46 +0600

    Min Max Shop

--- src/test/java/tests/MinMax.java
+++ src/test/java/tests/MinMax.java
@@ -28,7 +28,7 @@ public class MinMax {
 
     public void formtest() {
         try {
-            driver.navigate().to("http://minmax.test/wp-admin/");
+           /* driver.navigate().to("http://minmax.test/wp-admin/");
             driver.findElement(By.id("user_login")).sendKeys("admin");
             driver.findElement(By.id("user_pass")).sendKeys("admin");
             driver.findElement(By.id("wp-submit")).click();
@@ -38,19 +38,31 @@ public class MinMax {
 
 
             driver.findElement(By.id("wc_minmax_quantity_general_settings[min_product_quantity]")).clear();
-            driver.findElement(By.id("wc_minmax_quantity_general_settings[min_product_quantity]")).sendKeys("3");
+            driver.findElement(By.id("wc_minmax_quantity_general_settings[min_product_quantity]")).sendKeys("1");
 
             driver.findElement(By.id("wc_minmax_quantity_general_settings[max_product_quantity]")).clear();
             driver.findElement(By.id("wc_minmax_quantity_general_settings[max_product_quantity]")).sendKeys("8");
 
             driver.findElement(By.id("wc_minmax_quantity_general_settings[min_cart_price]")).clear();
-            driver.findElement(By.id("wc_minmax_quantity_general_settings[min_cart_price]")).sendKeys("40");
+            driver.findElement(By.id("wc_minmax_quantity_general_settings[min_cart_price]")).sendKeys("10");
 
             driver.findElement(By.id("wc_minmax_quantity_general_settings[max_cart_price]")).clear();
-            driver.findElement(By.id("wc_minmax_quantity_general_settings[max_cart_price]")).sendKeys("200");
-
+            driver.findElement(By.id("wc_minmax_quantity_general_settings[max_cart_price]")).sendKeys("100");
             driver.findElement(By.id("submit")).click();
+*/
+            driver.navigate().to("http://minmax.test/shop/");
+            driver.findElement(By.xpath("//body/section[@id='primary']/main[@id='main']/ul[1]/li[2]/a[2]")).click();
 
+            driver.findElement(By.xpath("//a[contains(text(),'View cart')]")).click();
+            driver.findElement(By.xpath("//a[contains(text(),'Proceed to checkout')]")).click();
+            driver.findElement(By.id("billing_first_name")).sendKeys("Mahbubur");
+            driver.findElement(By.id("billing_last_name")).sendKeys("Rahman");
+            driver.findElement(By.id("billing_address_1")).sendKeys("Dhaka");
+            driver.findElement(By.id("billing_city")).sendKeys("Mirpur");
+            driver.findElement(By.id("billing_phone")).sendKeys("01671816194");
+            driver.findElement(By.id("billing_email")).sendKeys("mahbub.095@gmail.com");
+
+            driver.findElement(By.id("place_order")).click();
 
         } catch (Exception e) {
             e.printStackTrace();
